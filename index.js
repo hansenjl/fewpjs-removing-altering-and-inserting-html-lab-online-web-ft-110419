ddebab708e135ec35cfc5adc0970872c3e659a47
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-02-20 02:06:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-let element = document.createElement('div')
-
-document.body.appendChild(element)
-
-let ul = document.createElement('ul')
- 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
- 
-element.appendChild(ul)
 
 let main = document.querySelector('main')
 main.remove()
