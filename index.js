ac8379a62f74fb4abef869300fcfccf64f73362c
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-02-20 02:06:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+let newHeader = document.createElement('h1#victory')
+newHeader.innerHTML = "YOUR-NAME is the champion"
+document.body.appendChild(newHeader)
 
 let main = document.querySelector('main')
 main.remove()
 
-let newHeader = document.createElement('h1#victory')
-newHeader.innerHTML = "YOUR-NAME is the champion"
-document.body.appendChild(newHeader)
